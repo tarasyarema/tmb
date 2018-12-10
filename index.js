4b908631846ce4e1f50b59cee6fdb4dfc62f8051
taras-laptop <tarasyaremabcn@gmail.com>
2018-12-10 22:02:10 +0100

    API working on

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
-const app = require('express');
+const express = require('express');
 const request = require('request');
 const Datastore = require('nedb');
+const app = express();
 require('dotenv').load();
 
-let auth = {
+const auth = {
     app_id: typeof(process.env.APP_ID) == 'string' ? process.env.APP_ID : null,
     app_key: typeof(process.env.APP_KEY) == 'string' ? process.env.APP_KEY : null,
 };
 
-let url = null;
 
 function format_url (info) {
-    if (info.num != 'number' || info.id != 'number') {
-        return 1;
+    if (typeof(info.num) != 'number' || typeof(info.id) != 'number') {
+        return null;
     } else {
-        url = `https://api.tmb.cat/v1/ibus/lines/${info.num}/stops/${info.id}`;
-        return 0;
+        return `https://api.tmb.cat/v1/ibus/lines/${info.num}/stops/${info.id}`;
     }
 }
 
-function get_data (info) {
+app.get('/api/:id/:num', (req, res) => {    
+    let info = {
+        num: parseInt(req.params.num),
+        id: parseInt(req.params.id),
+    };
+   
+    console.log(`GET /api/${info.id}/${info.num}`);
+
     if (auth.app_id == null || auth.app_key == null) {
         console.log('Auth error. Put your APP_ID and APP_KEY into a .env file.')
         process.exit(1);
     }
 
-    if (format_url(info)) {
+    let url = format_url(info);
+
+    if (typeof(url) == 'null') {
         console.log('Info error.')
         process.exit(1);
     }
 
+
     request.get({ uri: url, qs: auth }, 
-        (err, res, body) => {
-            if (err) return err;
+        (error, response, body) => {
+            if (error) return error;
 
             let data = JSON.parse(body).data.ibus[0];
             let time_s = data['t-in-s'];
             let time_m = data['t-in-min'];
-            console.log(`Time left for ${req_info.num} @ stop ${req_info.id}: ${time_m} min ${time_s - 60 * time_m} s.`)
-        });
-}
 
-let req_info = {
-    num: 54, 	// Bus line number
-    id: 208		// Stop id
-};
+            let time_left = {
+                min: time_m,
+                seg: time_s - 60 * time_m
+            }; 
 
-get_data(req_info);
+            console.log('  ▷ Response sent.')
+            res.send(`<h2> Time for ${info.num}</h2>\n${time_left.min} min ${time_left.seg} s.`);
+    });
+});
+
+app.listen(3000);
