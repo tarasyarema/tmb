e16190120e157738788b299631652e89e7777517
2pac <tarasyarema@pm.me>
2020-01-13 19:10:53 +0100

    added goroutine defer function

--- utils.go
+++ utils.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"log"
+	"sync"
 	"time"
 )
 
 func timeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
 	log.Printf("%s took %s", name, elapsed)
+}
+
+func deferDone(wg *sync.WaitGroup, start time.Time, id int, t *Times) {
+	wg.Done()
+
+	elapsed := time.Since(start)
+	t.Elapsed = elapsed.Milliseconds()
+
+	// log.Printf("Worker %v took %s", id, elapsed)
 }
